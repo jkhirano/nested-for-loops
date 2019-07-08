f90097b24899a4f181cf8a01b953b6a87869314a
Janelle K Hirano <jaahnelle@gmail.com>
2019-07-07 16:15:35 -1000

    removed console logs and commented code

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -9,23 +9,12 @@ function nestedForLoops(depth, width = depth) {
 
   // Write code here
 
-  //   let finalResult = "";
-
-  //   for (let x = 0; x < depth; x++) {
-  //     for (let y = 0; y < width); y++) {
-  //       finalResult += { 'x:' + x } + { 'y: '+ y };
-  //     }
-
-  // }
-
   let final = "";
 
   for (y = 0; y < depth; y++) {
     for (x = 0; x < width; x++) {
       let str = `{x:${x}, y:${y}}`;
-      // let str = "{x: " + [x] + ", y: " + [y] + "}";
       result += str;
-      // result = result + string;
       if (x < width - 1) {
         result += ", ";
       }
@@ -38,6 +27,3 @@ function nestedForLoops(depth, width = depth) {
 }
 
 // To see your console output outside the tests add function calls here.
-console.log(nestedForLoops(3));
-console.log(nestedForLoops(10, 5));
-console.log(nestedForLoops(5, 10));
